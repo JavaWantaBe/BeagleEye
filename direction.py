04aedba68c35474215d20567dd11445a09c41d1c
Richard Lowe <ocdrichard@gmail.com>
2015-04-13 22:26:38 -0700

    Fixed utf8 errors.

--- direction.py
+++ direction.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 __author__ = 'trey'
+
 
 import logging
 import cv2
@@ -29,9 +31,9 @@ def show_histogram(frame):
     # The zip function creates a permutation that consists of a combination of both arguments
 
     for item, col in zip([b, g, r], color):
-        """ cv2.calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
-        1.  images : it is the source image of type uint8 or float32. it should be given in square brackets, ie,
-            “[img]”.
+        """
+        cv2.calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
+        1.  images : it is the source image of type uint8 or float32. it should be given in square brackets, ie, [img].
         2.  channels : it is also given in square brackets. It the index of channel for which we calculate histogram.
             For example, if input is grayscale image, its value is [0]. For color image, you can pass [0],[1] or [2] to
             calculate histogram of blue,green or red channel respectively.
@@ -39,9 +41,11 @@ def show_histogram(frame):
             histogram of particular region of image, you have to create a mask image for that and give it as mask.
         4.  histSize : this represents our BIN count. Need to be given in square brackets. For full scale, we pass
             [256].
-        5.  ranges : this is our RANGE. Normally, it is [0,256]."""
+        5.  ranges : this is our RANGE. Normally, it is [0,256].
+        """
         hist_item = cv2.calcHist([item], [0], None, [256], [0, 255])
-        """ The function normalizes the histogram bins by scaling them so that the sum of the bins becomes equal to
+        """
+        The function normalizes the histogram bins by scaling them so that the sum of the bins becomes equal to
             factor.
         1.  src – input array.
         2.  dst – output array of the same size as src .
@@ -51,7 +55,7 @@ def show_histogram(frame):
         6.  dtype – when negative, the output array has the same type as src; otherwise, it has the same number of
             channels as src and the depth =CV_MAT_DEPTH(dtype).
         7.  mask – optional operation mask.
-            """
+        """
         cv2.normalize(hist_item, hist_item, 0, 255, cv2.NORM_MINMAX)
 
         hist = np.int32(np.around(hist_item))

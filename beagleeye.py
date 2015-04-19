05dcd3b8a0f4f99d56f2dbea4987eea2236b60fa
Richard Lowe <ocdrichard@gmail.com>
2015-04-19 01:17:03 -0700

    Working capture manager

--- beagleeye.py
+++ beagleeye.py
@@ -7,31 +7,21 @@ from os import path
 import direction
 import icu
 import ocr
+import time
 from settings import SettingManager
 import sys
-from threading import Thread, Timer
-import Queue
+from capmanager import CaptureManager
 
+on_beaglebone = True
 
-
-# *********************Web Video ************************
-#   The video file mjpg/video.mjpg is simply being accessed
-#   on a http server (this is a live feed)
-http_video_file = "http://myapplecam.com/mjpg/video.mjpg"
-
-# **********************Local Video *********************
-#   OpenCV can also read some saved video files but
-#   it'll take some work installing the codecs
-local_video_file = ""
+try:
+    import Adafruit_BBIO.GPIO as GPIO
+except ImportError:
+    on_beaglebone = False
 
 # *********************Settings Mangaer******************
 #   Settings manager
 local_settings = SettingManager()
-
-# ******************** Video Device *********************
-#   Capture device used
-capture_device = 0
-capture_queue = Queue.Queue(100)
 
 # ********************** Logger **************************
 #   Create logger with proper date and formatting for both
@@ -63,110 +53,61 @@ syslog = logging.getLogger('main')
 # ******************************************
 #               Functions
 # ******************************************
-def setup_capture_device(device):
-    global capture_device  # Accesses the global previously defined
-
-    if device == capture_device:
-        cam_settings = local_settings.get_settings('camera')
-
-        capture_device = cv2.VideoCapture(int(cam_settings['device']))
-
-        # Setup device
-        capture_device.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, int(cam_settings['width']))
-        capture_device.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, int(cam_settings['height']))
-        capture_device.set(cv2.cv.CV_CAP_PROP_FPS, int(cam_settings['fps']))
-    else:
-        capture_device = cv2.VideoCapture(device)
-
-    # Print current device settings
-    syslog.debug('width: {}, height: {},\
-    fps: {}'.format(str(capture_device.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
-                    str(capture_device.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)),
-                    str(capture_device.get(cv2.cv.CV_CAP_PROP_FPS))))
-
-
-##
-#   @brief Gets image from capture device
-#
-#   Retrieves a single frame from the configured capture device
-#
-#   @returns - frame
-def get_frame():
-    while not capture_queue.full():
-        ret, frame = capture_device.read()
-
-        if ret:
-            capture_queue.put(frame)
-            # cv2.waitKey(100)
-        else:
-            syslog.error("Failed to capture image")
-
-
 def print_usage():
     print """Usage: python beagleeye.py [h|l|u]
     h: http video file
     l: local video file
     u: usb or integrated camera device"""
+    exit(1)
 
 
 def main():
+    # *********************Web Video ************************
+    #   The video file mjpg/video.mjpg is simply being accessed
+    #   on a http server (this is a live feed)
+    http_video_file = "http://myapplecam.com/mjpg/video.mjpg"
 
     argument_length = len(sys.argv)
+    cam_settings = local_settings.get_settings('camera')
+    capture_device = 0
 
-    if argument_length == 1:
-        # no supplied argument is okay -- use default
-        video_file_type = ""
-    elif argument_length == 2:
+    if argument_length >= 2:
         # if there is a supplied argument, make sure there is only one
-        video_file_type = sys.argv[1]
-    else:
-        # otherwise print an error
-        print_usage()
-        exit(1)
-
-    # Note: in cv2 the VideoCapture function can be used to create feeds from
-    #   both a usb device or a file
-    capture = ""
-    if video_file_type == 'h':
-        capture = http_video_file
-    elif video_file_type == 'l':
-        capture = local_video_file
-    elif video_file_type == 'u':
-        capture = capture_device
-    elif video_file_type == "":
-        # no supplied
-        capture = capture_device
-    else:
-        print_usage()
-        exit(1)
-
-    setup_capture_device(capture)
+        if sys.argv[1] == 'h':
+            capture_device = CaptureManager(http_video_file, 100)
+        elif sys.argv[1] == 'l':
+            try:
+                capture = sys.argv[2]
+            except IndexError:
+                print_usage()
+            if not path.isfile(capture):
+                print "File Not found"
+                print_usage()
+            capture_device = CaptureManager(capture, 100)
+        elif sys.argv[1] == 'u':
+            try:
+                capture = int(sys.argv[2])
+            except IndexError:
+                capture = 0
+            if not isinstance(capture, (int, long)):
+                print_usage()
+            capture_device = CaptureManager(capture, 100, int(cam_settings['fps']))
+            capture_device.set_dimensions(int(cam_settings['height']), int(cam_settings['width']))
+        else:
+            print_usage()
+            exit(1)
 
     detected_movement = False   # Variable for determining when to exit loop
-    on_beaglebone = False
-
-    # Start a separate thread to collect video frames
-    video_thread = Thread(name='video_capture', target=get_frame)
-    video_thread.start()
+    # on_beaglebone = False
 
     syslog.info("system started")
+    capture_device.start()
 
     while True:
         # Check if coming or going
         while not detected_movement:
-            try:
-                image = capture_queue.get()
-
-                if not on_beaglebone:
-                    cv2.imshow('Debugging Window', image)
-                    direction.show_histogram(image)
-                    cv2.waitKey(10)
-
-                if direction.direction_detected(image):
-                    detected_movement = True
-            except Queue.Empty:
-                syslog.debug("Queue empty")
-
+            time.sleep(1)
+            print "Pass queue to direction"
         detected_movement = False
 
         # If check equals setting, start image recognition
@@ -176,15 +117,8 @@ def main():
         # If multiple OCR on multiple frames equal, have valid entry / exit
 
         # Save to database
-        database.insert_new_data("INSERT INTO checkin VALUES(NOW())")
+        # database.insert_new_data("INSERT INTO checkin VALUES(NOW())")
         # If exit key entered, exit
-        myin = raw_input()
-
-        if not on_beaglebone:
-            cv2.destroyAllWindows()
-
-        if myin == 'x':
-            break
 
 
 if __name__ == "__main__":
--- capmanager.py
+++ capmanager.py
@@ -26,10 +26,9 @@ __all__ = ['CaptureManager']
 
 class CaptureManager(Thread):
     """ CaptureManager used for continuous flow of I/O from device """
-    def __init__(self, device=0, queue_size=100, fps=10):
+    def __init__(self, device, queue_size=100, fps=10):
         Thread.__init__(self)  # Initialize parent
         # Private Variables
-        self._paused = True  # start out paused
         self._device = cv2.VideoCapture(device)
         self._queue = Queue.Queue(queue_size)
         self._width = self._device.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
@@ -40,35 +39,23 @@ class CaptureManager(Thread):
         cap_log.debug("Capture Manager instance created")
 
     def run(self):
-        self._paused = False
-
-        while not self._paused:
+        while True:
             s_time = time.time()
+            successful_capture, image = self._device.read()
 
-            if not self._queue.full():
-                successful_capture, image = self._device.read()
-
-                if successful_capture:
+            if successful_capture:
+                try:
                     self._queue.put(image)
-                    self._fps = 1 / (time.time() - s_time)
-                    time.sleep(1 / self._desired_fps)
-                else:
-                    cap_log.error("Could not capture from device")
+                except Queue.Full:
+                    pass
+                cv2.waitKey(1000 / self._desired_fps)
+                self._fps = 1 / (time.time() - s_time)
             else:
-                time.sleep(1 / self._desired_fps)
-        time.sleep(.1)
+                cap_log.error("Could not capture from device")
 
-    def resume(self):
-        self._paused = False
-
-    def pause(self):
-        self._paused = True         # make self block and wait
-
-    @property
     def get_size(self):
         return self._queue.qsize()
 
-    @property
     def get_image(self):
         try:
             return self._queue.get()
@@ -87,7 +74,15 @@ class CaptureManager(Thread):
         self._device.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, width)
         self._device.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, height)
 
+    @property
+    def get_queue(self):
+        return self._queue
+
     def purge_queue(self):
+        """
+        Function to purge the queue if needed.  This could happen if long delays happen on other
+        threads and the queue is filled with stale images
+        """
         while not self._queue.empty():
             try:
                 tmp = self._queue.get()
@@ -100,11 +95,11 @@ def test_cap():
     cap = CaptureManager(0, 10, 10)
     cap.start()
     print "Going into while loop"
-    time.sleep(1)
 
     while True:
         print "Happy: " + str(cap.get_fps)
-        cv2.imshow("Debugging", cap.get_image())
+        cv2.imshow("Debugger", cap.get_image())
+        cv2.waitKey(10)
 
 
 if __name__ == "__main__":

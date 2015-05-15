02e3b32cdbd7474dc9e7e74902565344b46cb013
walterthelapcat <treyudy3@gmail.com>
2015-05-15 12:19:53 -0700

    Added movie file, still working on presentation.

--- presentation.py
+++ presentation.py
@@ -2,32 +2,45 @@ __author__ = 'trey'
 
 import logging
 import cv2
-import numpy as np
+from threading import Thread
+from Queue import Queue
 import Tkinter
-from capmanager import CaptureManager
 import direction
 
 # Initialize logger for presentation
 presentlog = logging.getLogger('presentation')
 
 # Initialize capture manager, must start later though
-capture = CaptureManager(0, 100)
+
+
+def original(source, width, height, x, y):
+    cap = cv2.VideoCapture(source)
+    while True:
+        success, img = cap.read()
+        while success:
+            cv2.namedWindow("Original", cv2.WINDOW_FREERATIO)
+            cv2.resizeWindow("Original", width, (height)-55)  # top
+            cv2.imshow("Original", img)
+            cv2.moveWindow("Original", x, y)
+            cv2.waitKey(10)
+
+
+def detection(source, width, height, x, y):
+    capture = cv2.VideoCapture(source)
+    while True:
+        success, img = capture.read()
+
+
 
 def show_project():
 
-    capture.start()
-    my_queue = capture.get_queue
-    orig_queue = capture.get_queue
-    image, detected = direction.direction_detected(my_queue)
-    while True:
-        image, detected = direction.direction_detected(my_queue)
-        root = Tkinter.Tk()
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        cv2.namedWindow("Original", cv2.WINDOW_FREERATIO)
-        cv2.resizeWindow("Original", screen_width/2, (screen_height/2)-55)  # top
-        cv2.imshow("Original", orig_queue.get())
-        cv2.moveWindow("Original", screen_width/2, 0)
+    root = Tkinter.Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    print "Width  = " + str(screen_width)
+    print "Height = " + str(screen_height)
+    original(0, screen_width/2, (screen_height/2)-55, 0, 0)
+'''
         if detected:
             cv2.putText(image, 'Motion Detected!!', (10, 400), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255),2,cv2.LINE_AA)
             cv2.resizeWindow("Differential Image", screen_width/2, (screen_height/2)-30)  # bottom
@@ -35,6 +48,6 @@ def show_project():
             cv2.moveWindow("Differential Image", screen_width/2, screen_height/2)
             cv2.waitKey(1000)
             detected = 0
-
+'''
 if __name__ == "__main__":
     show_project()
